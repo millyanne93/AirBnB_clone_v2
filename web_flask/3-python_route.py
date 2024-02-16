29eff48735193e0567bddfe37dd20217076f4a0c
millyanne93 <nmillyanne20@gmail.com>
2024-02-15 23:07:20 -0800

    3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" A script that starts a Flask web application.
+"""
+
+from flask import Flask
+
+app = Flask("__name__")
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    Route handler function for the root URL.
+    Return a given string
+    """
+    return ("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    Route handler function for '/hbnb' URL.
+    Returns a given string
+    """
+    return ("HBNB")
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def cText(text):
+    """
+    Route handler function for '/c/<text>' URL.
+    Display C followed by the value of the text variable
+    """
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pythonText(text="is cool"):
+    """Display Python followed by the value of the text variable"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
